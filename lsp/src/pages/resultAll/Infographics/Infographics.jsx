70b08c4b2c03f9aa91233cee383f26510226bee8
Dmitriy Voronov <raVenquq@yandex.ru>
2022-06-03 14:53:13 +0400

    fix popup

--- lsp/src/pages/resultAll/Infographics/Infographics.jsx
+++ lsp/src/pages/resultAll/Infographics/Infographics.jsx
@@ -54,7 +54,6 @@ function Infographics() {
         return <Widget title={data.expression_name} value={data.value} iteration = {data.iteration}/>
     })
 
-        //<popup trigger={buttonPopupOM} setTrigger = {setButtonPopupOM}>
 
      return (
          <div className="inforaph">
@@ -65,128 +64,17 @@ function Infographics() {
                  <div className="named_exp_btn">
                      <button onClick={() => setButtonPopupNE(!buttonPopupNE)}>Named Expressions</button>
                  </div>
-                 <Popup shown={buttonPopupNE} close={() => {setButtonPopupNE(false);}}>
-                     <h3>
-                         <div className="widgets">
-                             <Widget type = "total_co2_emission"/> <Widget type = "total_initial_cost"/>
-                             <Widget type = "total_other_cost"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_co2_emission"/> <Widget type = "total_initial_cost"/>
-                             <Widget type = "total_other_cost"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_supply_cost"/> <Widget type = "total_carrying_cost"/>
-                             <Widget type = "total_tariffs"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_customer_tariffs"/> <Widget type = "total_inbound_cost"/>
-                             <Widget type = "total_outbound_cost"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_transportation_cost"/> <Widget type = "total_penalties"/>
-                             <Widget type = "total_revenued"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_production_costd"/> <Widget type = "total_closing_costd"/>
-                             <Widget type = "total_co2_emissiond"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_initial_costd"/> <Widget type = "total_other_costd"/>
-                             <Widget type = "total_supply_costd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_carrying_costd"/> <Widget type = "total_tariffsd"/>
-                             <Widget type = "total_customer_tariffsd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_inbound_costd"/> <Widget type = "total_outbound_costd"/>
-                             <Widget type = "total_transportation_costd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "total_penaltiesd"/>
-                         </div>
-                     </h3>
-                 </Popup>
-
 
                  <div className="obj_mmb_btn">
                      <button onClick={() => setButtonPopupOM(!buttonPopupOM)}>Objective Members</button>
                  </div>
-                 <Popup shown={buttonPopupOM} close={() => {setButtonPopupOM(false);}}>
-                     <h3>
-                         <div className="widgets">
-                             <Widget type = "inbound_processing_cost"/> <Widget type = "transportation_cost"/>
-                             <Widget type = "tariffs12"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "revenue12"/> <Widget type = "supply_cost"/>
-                             <Widget type = "outbound_processing_cost"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "initial_cost"/> <Widget type = "closing_cost"/>
-                             <Widget type = "co2_emission"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "penalties12"/> <Widget type = "production_cost"/>
-                             <Widget type = "other_cost"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "customer_tariffs"/> <Widget type = "carrying_cost"/>
-                             <Widget type = "inbound_processing_costd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "transportation_costd"/> <Widget type = "tariffs12d"/>
-                             <Widget type = "revenue12d"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "supply_costd"/> <Widget type = "outbound_processing_costd"/>
-                             <Widget type = "initial_costd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "closing_costd"/> <Widget type = "co2_emissiond"/>
-                             <Widget type = "penalties12d"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "production_costd"/> <Widget type = "other_costd"/>
-                             <Widget type = "customer_tariffsd"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget type = "carrying_costd"/>
-                         </div>
-                     </h3>
-                 </Popup>
-
 
                  <div className="over_stats_btn">
                      <button onClick={() => setButtonPopupOS(!buttonPopupOS)}>Overall Stats</button>
                  </div>
-                 <Popup shown={buttonPopupOS} close={() => {setButtonPopupOS(false);}}>
-                     <h3>
-                         <div className="widgets">
-                             <Widget13 type = "inbound_processing_cost13"/> <Widget13 type = "transportation_cost13"/>
-                             <Widget13 type = "tariffs13"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget13 type = "revenue13"/> <Widget13 type = "supply_cost13"/>
-                             <Widget13 type = "outbound_processing_cost13"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget13 type = "initial_cost13"/> <Widget13 type = "closing_cost13"/>
-                             <Widget13 type = "penalties13"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget13 type = "production_cost13"/> <Widget13 type = "other_cost13"/>
-                             <Widget13 type = "carrying_cost13"/>
-                         </div>
-                         <div className="widgets">
-                             <Widget13 type = "objective"/>
-                         </div>
-                     </h3>
-                 </Popup>
 
                  <div className="NEcharts">
-                     <NEChart data = {dataExpression.dataSet} dataName = "name" title dataKeyFirst="it#1" dataKeySecond="it#2"/>
+                     <NEChart data = {dataExpression.dataSet} dataName = "name" dataKeyFirst="it#1" dataKeySecond="it#2"/>
                  </div>
 
                  <div className="OMcharts">
@@ -196,10 +84,122 @@ function Infographics() {
                  <div className="OScharts">
                      <OSChart data = {dataExpression.dataSet} dataName = "name" dataKeyFirst="it#1" dataKeySecond="it#2"/>
                  </div>
-
-
              </div>
+
+
+             <Popup shown={buttonPopupNE} close={() => {setButtonPopupNE(false);}}>
+                 <h3>
+                     <div className="widgets">
+                         <Widget type = "total_co2_emission"/> <Widget type = "total_initial_cost"/>
+                         <Widget type = "total_other_cost"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_co2_emission"/> <Widget type = "total_initial_cost"/>
+                         <Widget type = "total_other_cost"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_supply_cost"/> <Widget type = "total_carrying_cost"/>
+                         <Widget type = "total_tariffs"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_customer_tariffs"/> <Widget type = "total_inbound_cost"/>
+                         <Widget type = "total_outbound_cost"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_transportation_cost"/> <Widget type = "total_penalties"/>
+                         <Widget type = "total_revenued"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_production_costd"/> <Widget type = "total_closing_costd"/>
+                         <Widget type = "total_co2_emissiond"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_initial_costd"/> <Widget type = "total_other_costd"/>
+                         <Widget type = "total_supply_costd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_carrying_costd"/> <Widget type = "total_tariffsd"/>
+                         <Widget type = "total_customer_tariffsd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_inbound_costd"/> <Widget type = "total_outbound_costd"/>
+                         <Widget type = "total_transportation_costd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "total_penaltiesd"/>
+                     </div>
+                 </h3>
+             </Popup>
+
+             <Popup shown={buttonPopupOM} close={() => {setButtonPopupOM(false);}}>
+                 <h3>
+                     <div className="widgets">
+                         <Widget type = "inbound_processing_cost"/> <Widget type = "transportation_cost"/>
+                         <Widget type = "tariffs12"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "revenue12"/> <Widget type = "supply_cost"/>
+                         <Widget type = "outbound_processing_cost"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "initial_cost"/> <Widget type = "closing_cost"/>
+                         <Widget type = "co2_emission"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "penalties12"/> <Widget type = "production_cost"/>
+                         <Widget type = "other_cost"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "customer_tariffs"/> <Widget type = "carrying_cost"/>
+                         <Widget type = "inbound_processing_costd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "transportation_costd"/> <Widget type = "tariffs12d"/>
+                         <Widget type = "revenue12d"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "supply_costd"/> <Widget type = "outbound_processing_costd"/>
+                         <Widget type = "initial_costd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "closing_costd"/> <Widget type = "co2_emissiond"/>
+                         <Widget type = "penalties12d"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "production_costd"/> <Widget type = "other_costd"/>
+                         <Widget type = "customer_tariffsd"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget type = "carrying_costd"/>
+                     </div>
+                 </h3>
+             </Popup>
+
+             <Popup shown={buttonPopupOS} close={() => {setButtonPopupOS(false);}}>
+                 <h3>
+                     <div className="widgets">
+                         <Widget13 type = "inbound_processing_cost13"/> <Widget13 type = "transportation_cost13"/>
+                         <Widget13 type = "tariffs13"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget13 type = "revenue13"/> <Widget13 type = "supply_cost13"/>
+                         <Widget13 type = "outbound_processing_cost13"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget13 type = "initial_cost13"/> <Widget13 type = "closing_cost13"/>
+                         <Widget13 type = "penalties13"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget13 type = "production_cost13"/> <Widget13 type = "other_cost13"/>
+                         <Widget13 type = "carrying_cost13"/>
+                     </div>
+                     <div className="widgets">
+                         <Widget13 type = "objective"/>
+                     </div>
+                 </h3>
+             </Popup>
          </div>
+
      );
 }
 
